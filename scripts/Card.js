f54e95205d534c55df50ed56c42b87f6ec9733d1
Natalia Botygina <n.botygina@mail.ru>
2022-04-20 14:33:02 +0300

    changes according 7 project

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,7 +1,7 @@
 export class Card {
   constructor(data, cardTemplate, openFullSizeImage) {
     this._name = data.name;
-    this._src = data.link;
+    this._link = data.link;
     this._cardTemplate = cardTemplate;
     this._openFullSizeImage = openFullSizeImage;
   }
@@ -18,7 +18,7 @@ export class Card {
   generateCard() {
     this._element = this._getTemplate();
     this._imgForm = this._element.querySelector(".element__image");
-    this._imgForm.src = this._src;
+    this._imgForm.src = this._link;
     this._imgTitle = this._element.querySelector(".element__title");
     this._imgTitle.textContent = this._name;
     this._likeButton = this._element.querySelector(".element__like-button");
