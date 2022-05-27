b1248b46aeea383d317eccaa410c98ae80768432
Natalia Botygina <n.botygina@mail.ru>
2022-05-27 16:56:17 +0300

    changes according to 9th project

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,6 @@
 export default class Popup {
   constructor(selectorPopup) {
-    this._selectorPopup = selectorPopup;
-    this._popupElement = document.querySelector(selectorPopup);
+    this._selectorPopup = document.querySelector(selectorPopup);
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
@@ -12,21 +11,18 @@ export default class Popup {
   }
 
   openPopup() {
-    this._popupElement.classList.add("popup_opened");
+    this._selectorPopup.classList.add("popup_opened");
     document.addEventListener("keydown", this._handleEscClose);
   }
 
   closePopup() {
-    this._popupElement.classList.remove("popup_opened");
+    this._selectorPopup.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose);
-  }
+  }  
 
   setEventListeners() {
-    this._popupElement.addEventListener("click", (evt) => {
-      if (
-        evt.target.classList.contains("popup__close-button") ||
-        evt.target.classList.contains("popup")
-      ) {
+    this._selectorPopup.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("popup__close-button") || evt.target.classList.contains("popup")) {
         this.closePopup();
       }
     });
